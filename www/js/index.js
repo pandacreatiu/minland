0d18ad4cafad4c1df6bb145f1153ee3a820aa2b5
pandacreatiu <david@pandacreatiu.com>
2016-01-27 16:54:00 +0100

    www14
    www14

--- www/js/index.js
+++ www/js/index.js
@@ -52,7 +52,15 @@ $(function() {
 
 
 function def_lang(iam){
-    if(iam) { localStorage.setItem("lang", iam); }
+
+    switch(iam) {
+        case 1: currentlang = 'esp';
+        break;
+        case 2: currentlang = 'eng';
+        break;
+    }
+
+    if(iam) { localStorage.setItem("lang", currentlang); }
 
     if(!localStorage.getItem("lang")) {  
         localStorage.setItem("lang", "esp");
@@ -62,49 +70,154 @@ function def_lang(iam){
         case 'esp':
             //menu emybaby
             $(".txt_mibebe").text("MI BEBÉ");
+            $(".cont_mibebe").text("Recuerda todos los datos del nacimiento del bebé guardándolos para siempre en su ficha y sigue su evolución en la ficha médica, el termómetro o la báscula.");
+            
             $(".txt_lafamilia").text("LA FAMILIA");
+            $(".cont_lafamilia").text("Añade los datos de todos los miembros de la familia y crea el árbol genealógico de tu pequeño desde la web.");
+            
             $(".txt_vigilabebes").text("VIGILABEBES");
+            $(".cont_vigilabebes").text("¿Deseas ver a tu bebé estés donde estés? Selecciona tu modelo de vigilabebés, configúralo y conecta con tu bebé desde tu PC, Tablet o Smartphone.");
+            
             $(".txt_termometro").text("TERMÓMETRO");
+            $(".cont_termometro").text("Registra la temperatura de tu bebé manual o automáticamente y sigue su evolución. Comparte los datos con el pediatra o los seres queridos.");
+            
             $(".txt_buddy").text("BEMYBUDDY");
+            $(".cont_buddy").text("¡Una gran fuente de emociones y sensaciones para el bebé! Infinitas posibilidades, con múltiples canciones y sonidos especialmente diseñados para los más pequeños y totalmente personalizable.");
+            
             $(".txt_calendario").text("CALENDARIO");
+            $(".cont_calendario").text("Porque sabemos que estás muy ocupado, te ayudamos a organizar las citas médicas, eventos familiares o fechas importantes en un calendario único.");
+            
             $(".txt_fichamedica").text("FICHA MÉDICA");
+            $(".cont_fichamedica").text("Registra toda la información médica de tu bebé: datos de peso, altura, y perímetro craneal y sigue su evolución comparándolos con los estándares de la OMS o enviándolos por email desde la web.");
+            
             $(".txt_albumfotos").text("ÁLBUM DE FOTOS");
+            $(".cont_albumfotos").text("Guarda las imágenes de cada momento especial que vivas con tu bebé para poder disfrutarlos con los tuyos.");
+            
             $(".txt_emyscale").text("EMYSCALE");
+            $(".cont_emyscale").text("De bebé a niño, ¡mantén su peso siempre controlado! Guarda automáticamente el peso de tu hijo y sigue su evolución");
+
+            $(".txt_ajustes").text("AJUSTES");
+            $(".cont_ajustes").text("Tu configuración personal. Y si necesitas ayuda contacta con Miniland, estaremos siempre a tu lado.");
+
             //home
             $(".home_title").text("FACILITA LA TAREA DE SER PADRES");
             $(".home_subtitle").text("escoge un producto compatible y te contamos como");
+            $(".descarga_en").text("DESCARGA EN:");
+
+            //productes
+            $(".digimonitor35touch").text('digimonitor 3.5"touch');
+            $(".digimonitor35plus").text('digimonitor 3.5"plus');
+            $(".digimonitor24plus").text('digimonitor 2.4"plus');
+            $(".spinipcam").text('spin IPCam');
+            $(".everywhereipcam").text('everywhere IPCam');
+            $(".thermoadvancedplus").text('thermoadvanced plus');
+            $(".emyscale").text('eMyScale');
+            $(".bemybuddy").text('beMyBuddy');
+            
+
+            //productos - textos videos
+            $(".conectate_con_tu_bebe").text("Conéctate con tu bebé a distancia");
+            $(".con_la_aplicacion_podras_ver").text("Con la aplicación gratuita eMyBaby puedes ver a tu bebé desde cualquier parte");
+            $(".desde_cualquier_plataforma").text("Desde cualquier plataforma, a través de smartphones, tablets y ordenador");
+            $(".a_cualquier_hora").text("A cualquier hora y en cualquier lugar");
+            $(".va_siempre_contigo").text("Va siempre contigo");
+            $(".sigue_los_movimientos").text("Sigue los movimientos del bebé por toda la habitación girando la cámara desde tu móvil o tablet");
+            $(".multitud_de_funciones").text("Multitud de funciones como hablar y escuchar a tu bebé");
+            $(".con_detencion_y_grabacion").text("Con detección y grabación de movimiento y captura de imágenes");
+            $(".permite_visualizar_hasta").text("Permite visualizar hasta 4 cámaras en un mismo dispositivo");
+            $(".con_bemybuddy_puedes").text("Con beMyBuddy, puedes grabar con tu propia voz las canciones o cuentos favoritos del bebé");
+            $(".podras_reprodocirlos_cuando").text("Podrás reproducirlos cuando quieras, ¡estés donde estés!");
+            $(".con_la_aplicacion_podras_relajar").text("Con la aplicación gratuita eMyBaby podrás relajar al bebé con una amplia gama de sonidos o elegir entre los archivos que tengas en tu dispositivo");
+            $(".tu_bebe_te_sentira").text("Tu bebé te sentirá siempre cerca, incluso cuando no estés a su lado");
+            $(".emyscale_conecta").text("eMyScale conecta con el smartphone o tablet para registrar las mediciones de peso en la aplicación eMyBaby");
+            $(".con_la_aplicacion_podras_seguimiento").text("Con la aplicación gratuita eMyBaby podrás llevar un seguimiento de la evolución del peso del bebé");
+            $(".en_emybaby_encontraras").text("En eMyBaby encontrarás todos los registros de peso y gráficos evolutivos de tu bebé");
+            $(".thermoadvanced_plus_conecta").text("thermoadvanced plus conecta con el smartphone o tablet para el registro y control de las temperaturas del bebé");
+            $(".con_la_aplicacion_podras_medicion").text("Con la aplicación gratuita eMyBaby además de la medición precisa y sin contacto, dispondrás del registro de las temperaturas siempre a mano");
+            
             //footer
             $(".footer_txt").text("Descubre sus infinitas posibilidades en eMyBaby.com"); 
             break;
        
-       case 'eng':
+        case 'eng':
             //menu emybaby
-            $(".txt_mibebe").text("MI BEBÉ");
-            $(".txt_lafamilia").text("LA FAMILIA");
-            $(".txt_vigilabebes").text("VIGILABEBES");
-            $(".txt_termometro").text("TERMÓMETRO");
+            $(".txt_mibebe").text("MY BABY");
+            $(".cont_mibebe").text("Remember all the information of the baby’s birth saving it forever on his record and follow his evolution on the medical record, the thermometer or the scale.");
+            
+            $(".txt_lafamilia").text("THE FAMILY");
+            $(".cont_lafamilia").text("Add the information of all the family members and create your baby’s family tree from the website.");
+            
+            $(".txt_vigilabebes").text("BABY MONITORS");
+            $(".cont_vigilabebes").text("Do you want to see your baby wherever you are? Choose your baby monitor model, set it and connect with your baby from your PC, Tablet or Smartphone.");
+            
+            $(".txt_termometro").text("THERMOMETER");
+            $(".cont_termometro").text("Register your baby’s temperature manually or automatically and follow his evolution. Share the information with the pediatrician or with your loved ones.");
+            
             $(".txt_buddy").text("BEMYBUDDY");
-            $(".txt_calendario").text("CALENDARIO");
-            $(".txt_fichamedica").text("FICHA MÉDICA");
-            $(".txt_albumfotos").text("ÁLBUM DE FOTOS");
+            $(".cont_buddy").text("A great source of emotions and feelings for the baby! Endless possibilities, with multiple songs and sounds specially designed for the little ones and completely customizable.");
+            
+            $(".txt_calendario").text("CALENDAR");
+            $(".cont_calendario").text("Because we know you are busy, we help you organize the medical appointments, family events or important dates in a unique calendar.");
+            
+            $(".txt_fichamedica").text("MEDICAL RECORD");
+            $(".cont_fichamedica").text("Register all your baby’s medical information: weight, height and cranial perimeter and follow his evolution comparing the data with the WHO standards or sending it by email from our website.");
+            
+            $(".txt_albumfotos").text("PHOTO ALBUM");
+            $(".cont_albumfotos").text("Save the pictures of every special moment lived with your baby to enjoy them with your loved ones.");
+            
             $(".txt_emyscale").text("EMYSCALE");
+            $(".cont_emyscale").text("From baby to child, keep his weight always under control! Save automatically your child’s weight and follow his evolution.");
+
+            $(".txt_ajustes").text("SETTINGS");
+            $(".cont_ajustes").text("Your personal settings. And if you need any help, get in touch with Miniland, we will always be by your side.");
+ 
             //home
-            $("#txt1").text("text en eng");
+            $(".home_title").text("MAKING PARENTS' LIFE A LOT EASIER");
+            $(".home_subtitle").text("Choose and discover our Emybaby products");
+            $(".descarga_en").text("DESCARGA EN:");
+
+            //productes
+            $(".digimonitor35touch").text('digimonitor 3.5"touch');
+            $(".digimonitor35plus").text('digimonitor 3.5"plus');
+            $(".digimonitor24plus").text('digimonitor 2.4"plus');
+            $(".spinipcam").text('spin IPCam');
+            $(".everywhereipcam").text('everywhere IPCam');
+            
+            //productos - textos videos
+            $(".conectate_con_tu_bebe").text("Get in touch with your baby from the distance");
+            $(".con_la_aplicacion_podras_ver").text("With the free app eMyBaby stay in contact with your baby from anywhere");
+            $(".desde_cualquier_plataforma").text("And from any platform, either through smartphones, tablets or computers");
+            $(".a_cualquier_hora").text("Anytime and anywhere");
+            $(".va_siempre_contigo").text("Always with you");
+            $(".sigue_los_movimientos").text("Watch your baby moving all over the room from your cell phone or tablet");
+            $(".multitud_de_funciones").text("It includes a wide range of features such as talking and listening to your baby");
+            $(".con_detencion_y_grabacion").text("With movement detection/recording and image capturing");
+            $(".permite_visualizar_hasta").text("It allows you to monitor up to 4 cameras from a single device");
+            $(".con_bemybuddy_puedes").text("beMyBudddy enables you to record your own voice to play your baby’s favourite songs and tales");
+            $(".podras_reprodocirlos_cuando").text("Play them whenever and wherever you are.");
+            $(".con_la_aplicacion_podras_relajar").text("Relax your baby with a wide range of standard and personalised sounds on eMyBaby");
+            $(".tu_bebe_te_sentira").text("Your baby will always feel you nearby, even if you are away");
+            $(".emyscale_conecta").text("Connect eMyScale with your smartphone or tablet and register your baby’s weighings through eMybaby");
+            $(".con_la_aplicacion_podras_seguimiento").text("Free app eMybaby gives you the chance to track the evolution of your baby’s weighings");
+            $(".en_emybaby_encontraras").text("eMyBaby tracks all your baby’s information related to weighing records and evolutive graphs");
+            $(".thermoadvanced_plus_conecta").text("Connect thermoadvanced plus with your smartphone or tablet and register the evolution of your baby’s temperatures");
+            $(".con_la_aplicacion_podras_medicion").text("Thermoadvanced plus guarantees accurate and contactless temperature measurements. Track all these results on eMybaby");
+            
+            //footer
+            $(".footer_txt").text("Know more about endless features at eMyBaby.com"); 
             break;
     } 
 }
 
 
-
 $(document).one('pagebeforecreate', function () {
 
-    //alert('one pagebeforecreate');
-   /* $.mobile.pageContainer.find("[data-role=page]").each(function () {
+    $.mobile.pageContainer.find("[data-role=page]").each(function () {
         var panel = '<div data-role="panel" id="leftpanel2" data-position="left" data-display="push" data-theme="a"> <div class="menu_logo"> <img src="images/emybaby_letter.png" height="27" width="152"> </div><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_mibebe.png" height="52" width="66"> </div><div class="menu_baby_txt txt_mibebe"> </div><div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_lafamilia.png" height="52" width="66"> </div><div class="menu_baby_txt txt_lafamilia"> </div> <div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_vigilabebes.png" height="52" width="66"> </div><div class="menu_baby_txt txt_vigilabebes"> </div><div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_termometro.png" height="52" width="66"> </div><div class="menu_baby_txt txt_termometro"> </div><div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_buddy.png" height="52" width="66"> </div><div class="menu_baby_txt txt_buddy"> </div><div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_calendario.png" height="52" width="66"> </div><div class="menu_baby_txt txt_calendario"> </div><div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_fichamedica.png" height="52" width="66"> </div><div class="menu_baby_txt txt_fichamedica"> </div><div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_albumfotos.png" height="52" width="66"> </div><div class="menu_baby_txt txt_albumfotos"> </div><div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a><a href=""><div class="menu_baby_item"><div class="menu_baby_img"> <img src="images/icon_emyscale.png" height="52" width="66"> </div><div class="menu_baby_txt txt_emyscale"> </div><div class="menu_baby_marca"> <img src="images/marca.png" height="21" width="13"> </div></div></a></div>';
         $(this).prepend(panel);
-        var panel_dre = '<div data-role="panel" id="rightpanel2" data-position="right" data-display="overlay" data-theme="b" style="background-color: #2D3854;"><div class="menu_idiomas"><a href="#rightpanel2"><div class="item_idioma" onclick="def_lang("esp");"><div class="item_idioma_flag"> <img src="images/esp.png" height="19" width="18"> </div><div class="item_idioma_txt"> ESPAÑOL </div></div></a><a href="#rightpanel2"> <div class="item_idioma" onclick="def_lang("eng");"><div class="item_idioma_flag"> <img src="images/eng.png" height="19" width="18"> </div><div class="item_idioma_txt"> ENGLISH </div></div></a></div></div>';
+        var panel_dre = '<div data-role="panel" id="rightpanel2" data-position="right" data-display="overlay" data-theme="b" style="background-color: #2D3854;"><div class="menu_idiomas"><a href="#rightpanel2"><div class="item_idioma" onclick="def_lang(1);"><div class="item_idioma_flag"> <img src="images/esp.png" height="19" width="18"> </div><div class="item_idioma_txt"> ESPAÑOL </div></div></a><a href="#rightpanel2"> <div class="item_idioma" onclick="def_lang(2);"><div class="item_idioma_flag"> <img src="images/eng.png" height="19" width="18"> </div><div class="item_idioma_txt"> ENGLISH </div></div></a></div></div>';
         $(this).prepend(panel_dre);
-    });*/
+    });
     
     def_lang(); 
 });
