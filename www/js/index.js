b4f86ddd18affe1e6ce3f4bec257b1759bcb4752
pandacreatiu <david@pandacreatiu.com>
2016-02-01 19:00:22 +0100

    www36
    www36

--- www/js/index.js
+++ www/js/index.js
@@ -55,8 +55,8 @@ $(function() {
         //loop: true
     });
 
-    /*var vid = document.getElementById("myVideo");
-    if (vid){ vid.play(); }*/
+    var vid = document.getElementById("myVideo");
+    if (vid){ vid.play(); }
 
 });
 
